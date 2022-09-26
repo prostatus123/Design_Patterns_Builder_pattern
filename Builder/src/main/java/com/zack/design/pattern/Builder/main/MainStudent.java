2decfe32bdd7094e957c312b7a88324a45d3d190
Nguyễn Minh Tuấn <59287970+prostatus123@users.noreply.github.com>
2022-09-26 09:16:04 +0700

    update code design pattern builder

--- Builder/src/main/java/com/zack/design/pattern/Builder/main/MainStudent.java
+++ Builder/src/main/java/com/zack/design/pattern/Builder/main/MainStudent.java
@@ -9,7 +9,8 @@ public class MainStudent {
     public static void main(String[] args) {
         StudentBuilder studentBuilder = new StudentConcreteBuilder()
                 .setFirstName("Tuan")
-                .setLastName("Nguyen Minh");
+                .setLastName("Nguyen Minh")
+                .setId("123");
 
         System.out.println(studentBuilder.build());
 
